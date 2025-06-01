f31bbaac65612b29cfaf4b2a45925f4e09295e13
sarojlimbu <saroj.limbu@ausnepit.com.au>
2025-06-01 22:49:45 +0545

    updating firebase config

--- src/utils/firebaseConfig.ts
+++ src/utils/firebaseConfig.ts
@@ -4,13 +4,22 @@ import { getDatabase } from "firebase/database";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-  databaseURL: process.env.NEXT_PUBLIC_FIREBASE_RTDB_URL,
+  // apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  // authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  // projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  // storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  // messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  // appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  // databaseURL: process.env.NEXT_PUBLIC_FIREBASE_RTDB_URL,
+
+  apiKey: "AIzaSyAC9dwyTC5m3tzKoANZ-Z4y01JCUaH6mMY",
+  authDomain: "i-connect-app-6ea52.firebaseapp.com",
+  projectId: "i-connect-app-6ea52",
+  storageBucket: "i-connect-app-6ea52.firebasestorage.app",
+  messagingSenderId: 710511808039,
+  appId: "1:710511808039:web:840b76d0ba19908d578802",
+  databaseURL:
+    "https://i-connect-app-6ea52-default-rtdb.asia-southeast1.firebasedatabase.app/",
 };
 
 const app = initializeApp(firebaseConfig);
